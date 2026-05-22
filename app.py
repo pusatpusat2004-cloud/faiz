81959efa619c76e5d09198c187302e40b6c37ff1
pusatpusat2004-cloud <pusatpusat2004@gmail.com>
2026-05-22 22:10:47 +0700

    Remove motivational note for low performance
    Removed motivational note for low performance.

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if submit_button:
         elif nilai_kelancaran == "Cukup (C)":
             catatan_motivasi = "Bagus, perbanyak murajaah lagi di rumah ya."
         else:
-            catatan_motivasi = "Kurang semangat ni, yuk lebih giat lagi setorannya!"
+            
             
         # Proses Simpan ke Excel
         data_baru = {
